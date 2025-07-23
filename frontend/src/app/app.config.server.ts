a12ecdff3c79a55d336e4e65014245785d065a5c
stenio medeiros <steniomedeiro85@gmail.com>
2025-07-22 22:51:16 -0300

    Corrige exportação do config no SSR

--- frontend/src/app/app.config.server.ts
+++ frontend/src/app/app.config.server.ts
@@ -1,17 +1,14 @@
-// src/app/app.config.ts
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { routes } from './app.routes';
-import { authInterceptor } from './auth/auth-interceptor'; // CORREÇÃO AQUI
+//src/app/app.config.server.ts
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 
-export const appConfig: ApplicationConfig = {
+import { appConfig } from './app.config';
+import { serverRoutes } from './app.routes.server';
+
+const serverConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideServerRendering(withRoutes(serverRoutes))
   ]
 };
+
+export const config = mergeApplicationConfig(appConfig, serverConfig);
