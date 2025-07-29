b4d3465d07e66e20a80f8e1d643b494440312d69
matheusnl <jmatheusnl@gmail.com>
2025-07-28 23:25:42 -0300

    fix: relacionamento Material e Question

--- backend/src/main/java/com/studyswap/backend/model/Material.java
+++ backend/src/main/java/com/studyswap/backend/model/Material.java
@@ -1,5 +1,8 @@
 package com.studyswap.backend.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -59,6 +63,9 @@ public class Material {
     @JoinColumn(name = "user_id", nullable = false)
     @NotNull(message = "O usuário é obrigatório")
     private User user;
+
+    @OneToMany(mappedBy = "material", cascade = CascadeType.ALL)
+    private List<Question> questions;
 
     public Material() {
     }
