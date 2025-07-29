54ba925c8b4a3bdcf688472212e16b098092e97c
matheusnl <jmatheusnl@gmail.com>
2025-07-28 23:34:23 -0300

    feat: criar MaterialService

--- backend/src/main/java/com/studyswap/backend/repository/MaterialRepository.java
+++ backend/src/main/java/com/studyswap/backend/repository/MaterialRepository.java
@@ -1,11 +1,10 @@
 package com.studyswap.backend.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.studyswap.backend.model.Material;
 
 @Repository
 public interface MaterialRepository extends JpaRepository<Material, Long>{
-    List<Material> findByUserId(Long userId);
+    
 }
---
+++ backend/src/main/java/com/studyswap/backend/service/MaterialService.java
@@ -0,0 +1,72 @@
+package com.studyswap.backend.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.studyswap.backend.dto.MaterialDTO;
+import com.studyswap.backend.model.Material;
+import com.studyswap.backend.model.User;
+import com.studyswap.backend.repository.MaterialRepository;
+import com.studyswap.backend.repository.UserRepository;
+
+@Service
+public class MaterialService {
+    @Autowired
+    private MaterialRepository materialRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Material createMaterial(MaterialDTO materialDTO){
+        Material entity = convertToEntity(materialDTO);
+        return materialRepository.save(entity);
+    }
+
+    public Material updateMaterial(Long idMaterial, MaterialDTO materialDTO) {
+        Material entity = materialRepository.findById(idMaterial).orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Material não encontrado"));
+        
+        copyDTOToEntity(materialDTO, entity);
+        return materialRepository.save(entity);
+    }
+
+    public Material getMaterialById(Long id) {
+        return materialRepository.findById(id).orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Material não encontrado"));
+    }
+
+    public List<Material> getAllMaterials() {
+        return materialRepository.findAll();
+    }
+
+    public void deleteMaterial(Long id) {
+        Material entity = materialRepository.findById(id).orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Material não encontrado"));
+        
+        materialRepository.delete(entity);
+    }
+
+    private Material convertToEntity(MaterialDTO materialDTO) {
+        Material entity = new Material();
+        copyDTOToEntity(materialDTO, entity);
+        return entity;
+    }
+
+    // Copia os dados de um MaterialDTO para uma entidade Material
+    private void copyDTOToEntity(MaterialDTO materialDTO, Material entity) {
+        entity.setTitle(materialDTO.getTitle());
+        entity.setDescription(materialDTO.getDescription());
+        entity.setMaterialType(materialDTO.getMaterialType());
+        entity.setConservationStatus(materialDTO.getConservationStatus());
+        entity.setTransactionType(materialDTO.getTransactionType());
+        entity.setPrice(materialDTO.getPrice());
+        entity.setPhoto(materialDTO.getPhoto());
+        User user = userRepository.findById(materialDTO.getUserId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado"));
+        entity.setUser(user);
+    }
+}
