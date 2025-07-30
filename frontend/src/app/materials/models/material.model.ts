13a46b3ec1c99f0d5bd6c782fcd70539fb81af43
luis-fil <luis.fbferreira@ufape.edu.br>
2025-07-30 07:33:05 -0300

    feat: Service e Material (objeto) no frontend

---
+++ frontend/src/app/materials/models/material.model.ts
@@ -0,0 +1,30 @@
+export interface Material {
+    id?: number;
+    title: string;
+    description?: string;
+    materialType: MaterialType;
+    conservationStatus: ConservationStatus;
+    transactionType: TransactionType;
+    price?: number | null;
+    photoUrl?: string;
+}
+
+export enum MaterialType {
+    LIVRO = 'LIVRO',
+    APOSTILA = 'APOSTILA',
+    EQUIPAMENTO = 'EQUIPAMENTO',
+    MOBILIARIO = 'MOBILIARIO'
+}
+
+export enum ConservationStatus {
+    NOVO = 'NOVO',
+    BOM = 'BOM',
+    RAZOAVEL = 'RAZOAVEL',
+    VELHO = 'VELHO'
+}
+
+export enum TransactionType {
+    DOACAO = 'DOACAO',
+    TROCA = 'TROCA',
+    VENDA = 'VENDA'
+}
