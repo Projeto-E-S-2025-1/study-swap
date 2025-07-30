40347e618c4fda2cbdf13dad683cab221da4705c
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2025-07-29 23:40:08 -0300

    refactor: atualizar searchByTitleAndType para searchByFilters e para comportar novos filtros (conservationStatus, transactionType)

--- backend/src/main/java/com/studyswap/backend/repository/MaterialRepository.java
+++ backend/src/main/java/com/studyswap/backend/repository/MaterialRepository.java
@@ -6,13 +6,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.studyswap.backend.model.ConservationStatus;
 import com.studyswap.backend.model.Material;
 import com.studyswap.backend.model.MaterialType;
+import com.studyswap.backend.model.TransactionType;
 
 @Repository
 public interface MaterialRepository extends JpaRepository<Material, Long>{
     @Query("SELECT m FROM Material m " +
            "WHERE (:title IS NULL OR LOWER(m.title) LIKE LOWER(CONCAT('%', :title, '%'))) " +
-           "AND (:materialType IS NULL OR m.materialType = :materialType)")
-    List<Material> searchByTitleAndType(@Param("title") String title, @Param("materialType") MaterialType materialType);
+           "AND (:materialType IS NULL OR m.materialType = :materialType) " +
+           "AND (:conservationStatus IS NULL OR m.conservationStatus = :conservationStatus) " +
+           "AND (:transactionType IS NULL OR m.transactionType = :transactionType)")
+    List<Material> searchByFilters(
+        @Param("title") String title,
+        @Param("materialType") MaterialType materialType,
+        @Param("conservationStatus") ConservationStatus conservationStatus,
+        @Param("transactionType") TransactionType transactionType
+    );
 }
