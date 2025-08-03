2b60469660ee7341163b276f03b77ab95ba62d67
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2025-08-03 19:32:00 -0300

    feat: criar componente question list

--- frontend/src/app/materials/models/question.model.ts
+++ frontend/src/app/materials/models/question.model.ts
@@ -1,11 +1,8 @@
-import { Material } from '../../materials/models/material.model';
-import { User } from '../../models/user.model';
-
 export interface Question {
   id?: number;
   title: string;
   description: string;
-  author: User;
-  material: Material;
+  authorName: string;
   createdAt?: string;
 }
+
