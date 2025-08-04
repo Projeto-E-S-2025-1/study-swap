cd1fe49954d8c858f0c7201064265aec6d4b6f19
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2025-08-03 22:51:40 -0300

    refactor: atualizar interface da question para adicionar author

--- frontend/src/app/materials/models/question.model.ts
+++ frontend/src/app/materials/models/question.model.ts
@@ -2,6 +2,10 @@ export interface Question {
   id: number;
   title: string;
   description: string;
+  author?: {
+    id: number;
+    name: string;
+  };
   authorName: string;
   authorId: number;
   createdAt?: string;
