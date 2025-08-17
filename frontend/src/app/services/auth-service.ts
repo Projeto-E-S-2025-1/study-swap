174d990a61a0e981d1d505c727646a97285c350f
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2025-08-17 14:32:12 -0300

    refactor: atualizar material service para usar variáveis de ambiente

--- frontend/src/app/services/auth-service.ts
+++ frontend/src/app/services/auth-service.ts
@@ -2,17 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError } from 'rxjs';
-
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 //
 export class AuthService {
-  private apiUrl = `http://localhost:8080`;
+  private apiUrl = environment.apiUrl;
   private http = inject(HttpClient);
   router = inject(Router);
-  
+
   login(email: string, password: string): void {
     this.http.post<any>(`${this.apiUrl}/login`, {email, password})
       .pipe(
