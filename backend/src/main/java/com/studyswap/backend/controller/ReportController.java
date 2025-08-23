aea5f4767a4d388e96f09dbf6d2ced4a9ce0484e
jeasiel <jeasielabner@gmail.com>
2025-08-23 19:34:01 -0300

    fix: corrigida issue de injeção de dependência apontada pelo sonar no ReportController

--- backend/src/main/java/com/studyswap/backend/controller/ReportController.java
+++ backend/src/main/java/com/studyswap/backend/controller/ReportController.java
@@ -6,7 +6,6 @@ import com.studyswap.backend.service.ReportService;
 
 import jakarta.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,8 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/report")
 public class ReportController {
 
-    @Autowired
     private ReportService reportService;
+
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
 
     @PostMapping
     public ResponseEntity<ReportResponseDTO> createReport(@Valid @RequestBody CreateReportDTO dto) {
