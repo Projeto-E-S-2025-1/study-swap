012933faed3192e0ace112ffc303639f9899c30c
jeasiel <jeasielabner@gmail.com>
2025-08-23 20:46:55 -0300

    fix: corrigida issue de injeção de dependência apontada pelo Sonar no MaterialService e import no AuthService

--- backend/src/main/java/com/studyswap/backend/service/AuthService.java
+++ backend/src/main/java/com/studyswap/backend/service/AuthService.java
@@ -1,6 +1,5 @@
 package com.studyswap.backend.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
