132207c5959b9dfab44628e1d43f4e13f5bb7e40
DavidSaymmon <sawewfelipe@gmail.com>
2025-08-24 17:23:41 -0300

    feat: cria controller da Transaction

---
+++ backend/src/main/java/com/studyswap/backend/controller/TransactionController.java
@@ -0,0 +1,51 @@
+package com.studyswap.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.studyswap.backend.dto.TransactionResponseDTO;
+import com.studyswap.backend.service.TransactionService;
+
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+	@Autowired
+	private TransactionService transactionService;
+	
+	@DeleteMapping("/{idTransaction}")
+	public ResponseEntity<Void> cancelTransaction(Authentication auth, @PathVariable("idTransaction") Long idTransaction){
+		transactionService.cancelTransaction(auth, idTransaction);
+		return ResponseEntity.noContent().build();		
+	}
+	@PostMapping("/material/{idMaterial}")
+	public ResponseEntity<TransactionResponseDTO>createTransaction(Authentication auth, @PathVariable("idMaterial") Long idMaterial){
+		return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.createTransaction(auth, idMaterial));
+	}
+	@PutMapping("/{idTransaction}")
+	public ResponseEntity<TransactionResponseDTO> completeTransaction(Authentication auth, 
+			@PathVariable("idTransaction") Long idTransaction){
+		return ResponseEntity.status(HttpStatus.OK).body(
+				transactionService.completeTransaction(auth, idTransaction));
+	}
+	
+	@GetMapping("/material/{idMaterial}")
+	public ResponseEntity<List<TransactionResponseDTO>> findAllTransactionsByMaterial(Authentication auth, 
+			@PathVariable("idMaterial") Long idMaterial){
+		return ResponseEntity.status(HttpStatus.OK).body(
+				transactionService.findAllTransactionsByMaterial(auth, idMaterial));
+	}
+
+}
+
