298c85edb0e326fe92f4b8390b9f54fba8f7141d
luis-fil <luis.fbferreira@ufape.edu.br>
2025-08-25 22:21:55 -0300

    feat: modulo para listar transações que o usuário participa (frontend)

---
+++ frontend/src/app/transaction/components/list-user-transactions/list-user-transactions.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { TransactionService } from '../../services/transaction.service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-transactions',
+  templateUrl: './list-user-transactions.html',
+  styleUrls: ['./list-user-transactions.css'],
+  imports: [RouterModule, CommonModule]
+})
+export class TransactionsComponent implements OnInit {
+  transactions: any[] = [];
+
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.loadTransactions();
+  }
+
+  loadTransactions() {
+    this.transactionService.getTransactionsByUser().subscribe({
+      next: (data) => {
+        this.transactions = data;
+      },
+      error: (err) => {
+        console.error('Erro ao carregar transações', err);
+      }
+    });
+  }
+}
