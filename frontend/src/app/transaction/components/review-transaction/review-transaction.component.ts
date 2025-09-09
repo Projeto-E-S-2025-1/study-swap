edb9546b0b2f554f530edd457617cca1747aaa6e
luis-fil <luis.fbferreira@ufape.edu.br>
2025-09-08 22:49:35 -0300

    Fix: Estrutura de review para compradores

--- frontend/src/app/transaction/components/review-transaction/review-transaction.component.ts
+++ frontend/src/app/transaction/components/review-transaction/review-transaction.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ReviewService } from '../../services/review.service';
+import { AuthService } from '../../../auth/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-review-form',
@@ -8,10 +11,14 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./review-transaction.component.css'],
   imports: [CommonModule, FormsModule]
 })
-export class ReviewFormComponent {
+export class ReviewFormComponent implements OnInit {
+  transactionId!: number;
+
   reviewDTO = {
+    authorId: 0,
     rating: 0,
-    comment: ''
+    description: '',
+    transactionId: 0
   };
 
   stars = [1, 2, 3, 4, 5];
@@ -19,6 +26,24 @@ export class ReviewFormComponent {
 
   successMessage: string | null = null;
   errorMessage: string | null = null;
+  userId: number | null = null;
+
+  constructor(
+    private reviewService: ReviewService,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.transactionId = Number(this.route.snapshot.paramMap.get('id'));
+    this.userId = this.authService.getUserId();
+    if (this.userId == null) {
+    throw new Error("Usuário não identificado");
+    }
+    this.reviewDTO.authorId = this.userId;
+    this.reviewDTO.transactionId = this.transactionId;
+  }
 
   setRating(rating: number) {
     this.reviewDTO.rating = rating;
@@ -29,11 +54,26 @@ export class ReviewFormComponent {
   }
 
   onSubmit() {
-    if (this.reviewDTO.rating > 0 && this.reviewDTO.comment.trim()) {
-      this.successMessage = "Avaliação enviada com sucesso!";
-      this.errorMessage = null;
-      // Aqui poderia chamar um service para salvar no backend
-      this.reviewDTO = { rating: 0, comment: '' };
+    if (!this.userId) {
+      this.errorMessage = "Usuário não identificado";
+      return;
+    }
+
+    if (this.reviewDTO.rating > 0 && this.reviewDTO.description.trim()) {
+      this.reviewService.create(this.transactionId, this.reviewDTO).subscribe({
+        next: () => {
+          this.successMessage = "Avaliação enviada com sucesso!";
+          this.errorMessage = null;
+          this.reviewDTO.rating = 0;
+          this.reviewDTO.description = '';
+          this.hoveredRating = 0;
+          this.router.navigate(['/material/'])
+        },
+        error: () => {
+          this.errorMessage = "Erro ao enviar avaliação.";
+          this.successMessage = null;
+        }
+      });
     } else {
       this.errorMessage = "Preencha todos os campos antes de enviar.";
       this.successMessage = null;
--- frontend/src/app/transaction/models/review.model.ts
+++ frontend/src/app/transaction/models/review.model.ts
@@ -1,10 +1,15 @@
 export interface Review {
     id: number;
-    author: {
-        id: number;
-        name: string;
-    }
+    authorName: string;
+    authorId: number;
+    materialTitle: string;
     rating: number;
     description: string;
     createdAt?: string;
+}
+
+export interface UserAverage {
+    userId: number;
+    averageRating: number;
+    totalReviews: number;
 }
